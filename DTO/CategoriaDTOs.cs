f131c94b558171e67869d70af003b0cb58d59680
Thiago <thiagolopesnonato@gmail.com>
2019-09-20 22:14:44 -0300

    Repositório e DTO

--- DTO/CategoriaDTOs.cs
+++ DTO/CategoriaDTOs.cs
@@ -2,5 +2,10 @@ namespace Bankastico.Data
 {
 	public class CategoriaDTOs
 	{
+		public class InsertCategoriaDTO
+		{
+			public int Id { get; set; }
+			public string Descricao { get; set; }
+		}
 	}
 }
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bankastico.Data;
+using Bankastico.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,20 +28,19 @@ namespace Bankastico
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddScoped<DataContext, DataContext>();
+            services.AddTransient<DespesaRepository, DespesaRepository>();
+            services.AddTransient<CategoriaRepository, CategoriaRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
             app.UseMvc();
