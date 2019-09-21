9bd5a35d5fafb6a3c4de6b0b3b5a30af7db8de9d
Thiago <thiagolopesnonato@gmail.com>
2019-09-20 22:18:33 -0300

    Correção no DTO de categoria

--- DTO/CategoriaDTOs.cs
+++ DTO/CategoriaDTOs.cs
@@ -5,7 +5,7 @@ namespace Bankastico.Data
 		public class InsertCategoriaDTO
 		{
 			public int Id { get; set; }
-			public string Descricao { get; set; }
+			public string Titulo { get; set; }
 		}
 	}
 }
